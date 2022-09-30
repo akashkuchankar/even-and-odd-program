580469d05e1feaaca9969e5de2fad360a022e013
akashkuchankar <kuchankarakash@gmail.com>
2022-09-30 04:10:54 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N % 2 ==0) 
+            {
+                Console.WriteLine("this es even number")
+                    ;
+
+                    ;
+            }
+            else
+            {
+                Console.WriteLine("this is odd number");
+
+            }
+            
+        }
+    }
+}
